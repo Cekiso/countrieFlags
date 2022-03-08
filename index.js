035706261b7edfbb74102a0520df1bf3965e0c92
Cekiso <nonkululeko@quirky30.co.za>
2022-03-08 10:25:54 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const button = document.querySelector('.submit');
 const CountryName = document.getElementById("CountryName");
-const Country = document.querySelector('.Country')
+const Country = document.querySelector('.Country');
+const newValue = document.querySelector('.newValue');
 const countries = ["Argentina", "Brazil", "Chile", "Zambia", "Uganda", "Malawi", "Rwanda", "Ireland", "Switzerland"];
 const flags = ["🇦🇷", "🇧🇷", "🇨🇱", "🇿🇲", "🇺🇬", "🇲🇼", "🇷🇼", "🇮🇪", "🇨🇭"];
 
-
+countries.sort((a, b) => a.localeCompare(b));
 countries.forEach((element, index) => {
     const emoji = flags[index];
 
@@ -14,17 +15,32 @@ countries.forEach((element, index) => {
 })
 
 
-function countrieBox(element) {
 
+function countrieBox() {
+    let flag = 'No flag selected'
     const boxvalue = document.getElementById("CountryName").value;
     countries.push(boxvalue);
+
+    const select = document.getElementById("countrieFlags");
+    // let value = select.options[select.selectedIndex].value;
+
+    if (select.selectedIndex > 0) {
+        flag = select.item(select.selectedIndex).textContent;
+    }
+
+
     const displayCountry = document.createElement("ul");
-    displayCountry.innerHTML = `${element}`;
+    displayCountry.innerHTML = boxvalue;
+
+
     document.body.appendChild(displayCountry);
 
     console.log(countries);
 
 
 }
+
+
+
 
 button.addEventListener('click', countrieBox)
